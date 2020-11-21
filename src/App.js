c97ed817fe8682629270a107a929f8e50d99904a
tanakorn0314 <tanakorn0412@gmail.com>
2020-11-21 15:04:50 +0700

    update

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const StyledWrapper = styled.div`
   }
 `;
 
-const types = [
+const typeOptions = [
   { label: 'noun', value: 'noun' },
   { label: 'verb', value: 'verb' },
   { label: 'adjective', value: 'adjective' },
@@ -32,10 +32,11 @@ function App() {
 
   const vocabs = [{ "meanings": ["มาพร้อมกับ"], "word": "Accompany", "createdAt": "2020-11-15T11:56:52.792Z", "types": ["verb"] }, { "meanings": ["การเข้าซื้อกิจการ", "การเข้าถือสิทธิ์", "การครอบครอง"], "types": ["noun"], "createdAt": "2020-11-17T06:39:36.046Z", "word": "acquisition" }, { "word": "complaint", "meanings": ["การร้องเรียน", "การร้องทุกข์", "การคร่ำครวญ"], "types": ["noun"], "createdAt": "2020-11-17T07:05:40.824Z" }, { "word": "complying", "types": ["noun"], "createdAt": "2020-11-17T07:08:25.211Z", "meanings": ["การยอมตาม", "การยินยอม", "การปฏิบัติตาม"] }, { "createdAt": "2020-11-17T05:40:43.548Z", "word": "correspondence", "types": ["noun"], "meanings": ["จดหมายโต้ตอบ", "ความเหมือนกัน"] }, { "word": "delegate", "meanings": ["ตั้งตัวแทน", "มอบหน้าที่"], "types": ["verb"], "createdAt": "2020-11-16T16:44:23.992Z" }, { "types": ["verb"], "word": "deserve", "meanings": ["สมน้ำสมเนื้อ", "สมควรได้รับ"], "createdAt": "2020-11-16T16:41:43.017Z" }, { "meanings": ["พยายาม", "มุมานะ", "อุตส่าห์"], "word": "endeavor", "types": ["verb"], "createdAt": "2020-11-17T06:03:36.413Z" }, { "types": ["verb"], "word": "engage", "createdAt": "2020-11-17T05:57:17.307Z", "meanings": ["มีส่วนร่วม", "ว่าจ้าง"] }, { "createdAt": "2020-11-17T07:06:42.122Z", "types": ["noun"], "word": "grievance", "meanings": ["ความไม่พอใจ", "ความเศร้าโศก", "ความขัดข้องใจ"] }, { "types": ["adverb"], "meanings": ["โดยนัยนี้", "ด้วยเหตุนี้", "โดยวิธีนี้"], "createdAt": "2020-11-17T05:37:12.526Z", "word": "hereby" }, { "types": ["adjective"], "createdAt": "2020-11-15T11:56:25.637Z", "meanings": ["ถูกต้องตามกฎหมาย"], "word": "legitimate" }, { "word": "merger", "meanings": ["การควบรวบกิจการ"], "createdAt": "2020-11-17T06:38:39.071Z", "types": ["noun"] }, { "word": "meticulous", "createdAt": "2020-11-17T07:47:36.831Z", "types": ["adjective"], "meanings": ["พิถีพิถัน", "เข้มงวด", "กวดขัน"] }, { "createdAt": "2020-11-17T06:05:44.594Z", "types": ["noun"], "meanings": ["ขั้นตอน", "กระบวนการ", "ระเบียบการ"], "word": "procedure" }, { "meanings": ["พิถีพิถัน", "แม่นยำ", "รุนแรง"], "word": "rigorous", "createdAt": "2020-11-17T07:49:20.488Z", "types": ["adjective"] }]
 
-  // let number = 10;
-  const [number, setNumber] = useState(10);
-
   const [dataList, setDataList] = useState(vocabs);
+
+  const [word, setWord] = useState('');
+  const [types, setTypes] = useState([]);
+  const [meanings, setMeanings] = useState('');
 
   const handleClick = () => {
     setDataList([...dataList, {
@@ -55,7 +56,10 @@ function App() {
           <div>
             <label htmlFor='word-input'>Word</label>
           </div>
-          <Input id='word-input' placeholder="English word" />
+          <Input
+            id='word-input'
+            placeholder="English word"
+          />
         </div>
 
         <div className='form-item'>
@@ -65,7 +69,7 @@ function App() {
           <Select
             className='select-types'
             id='select-types'
-            options={types}
+            options={typeOptions}
             mode="multiple"
             placeholder="Select types"
           />
@@ -81,9 +85,7 @@ function App() {
 
       </div>
 
-      <Button onClick={handleClick}>Add vocab</Button>
-      <br />
-      <br />
+      <Button className='form-item' type='primary' onClick={handleClick}>Add vocab</Button>
 
       <Row gutter={[16, 24]}>
         {dataList.map((item, index) => {
